81ef44f63008725ef6632535b1492a2abcb955fc
Mariano <mariano_farace@hotmail.com>
2021-12-04 14:10:20 +0100

    Remove Empty lines

--- decorator.js
+++ decorator.js
@@ -1,9 +1,6 @@
 //Crea un Decorator en un archivo que devuelva una función. Esta función realizará una conversión de moneda a euros multiplicando por el coeficiente de conversión del archivo adjunto currency_conversions.json  en función de la divisa original
 
-
 //Creo que hay un millon de formas de hacerlo, aca explico el razonamiento de la mia. Tengo una funcion simple (applyFactor) que hace una multiplicacion de dos factores. Quiero extender la funcionalidad para que haga una conversion de precios desde distintas monedas a euros, y para lograr esto voy a hacerlo con un decorator. Otro decorator podria por ejemplo, extender la funcionalidad para calcular los impuestos de un producto. Como el caso es sencillo, la implementacion del decorator solo requiere una funcion dentro de otra funcion. 
-
-
 
 const conversions = require('./currency_conversions.json')
 
@@ -16,8 +13,6 @@ let currencyToEuroDecorator = (apply, currencyAcronym, price) => {
     return apply(price, conversionFactorValue)
 
 }
-
-
 
 //------------------------------------------------------------------------------------
 
@@ -34,7 +29,6 @@ const priceListToBeConvertedToEuro = [
     { productName: "watermelon", price: 7, currencyAcronym: "CNY" }
 
 ]
-
 
 let convertListOfPricesToEuro = (listOfPrices) => {
     const listOfPricesInEU = listOfPrices.map(v => ({...v, priceInEU: currencyToEuroDecorator(applyFactor, v.currencyAcronym, v.price) }))
