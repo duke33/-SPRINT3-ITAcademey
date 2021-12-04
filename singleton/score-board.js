ea96d33bab25e308e265a84d86c16d4382b01827
Mariano <mariano_farace@hotmail.com>
2021-12-04 13:26:19 +0100

    Refactor the class

--- singleton/score-board.js
+++ singleton/score-board.js
@@ -1,5 +1,3 @@
-//Construye una aplicación que cree varios Jugadores. Los jugadores podrán ser añadidos a un juego, que mostrará un marcador con las puntuaciones y el ganador. La aplicación debe poder añadir o quitar puntos a cada jugador para que el marcador cambie. La clase Marcador debe implementar un patrón Singleton como requisito indispensable.
-
 let instance = null;
 
 class ScoreBoard {
@@ -9,21 +7,12 @@ class ScoreBoard {
         if (!instance) {
             instance = this;
         }
-
         return instance;
     }
 
     //Methods
 
-    joinGame(name) {
-        if (this.board.some(e => e.name === name)) {
-            console.log(`${name} player has already joined the Game!`)
-        } else {
-            this.board.push({ name: name, points: 0 })
-        }
-    }
-
-    addOrSubstractPoints(name, pointsToAdd) {
+    addOrSubtractPoints(name, pointsToAdd) {
         let currentPlayerIndex = this.board.findIndex(obj => name === obj.name);
         this.board[currentPlayerIndex].points += pointsToAdd
     }
@@ -38,7 +27,7 @@ class ScoreBoard {
         console.log(`The Winer is ${this.sort()[0].name}!!!!!!`)
     }
 
-    showScore() { //TODO
+    showScore() {
         console.log("Score Board: ",
             this.sort())
 
@@ -47,25 +36,9 @@ class ScoreBoard {
 }
 
 //Checking the class behaves as a Singleton:
-const checkSingleton = () => {
-    console.log("Class behaves as a singleton: ", (new ScoreBoard().value === new ScoreBoard().value) ? true : false)
-}
-checkSingleton()
+// const checkSingleton = () => {
+//     console.log("Class behaves as a singleton: ", (new ScoreBoard().value === new ScoreBoard().value) ? true : false)
+// }
+// checkSingleton()
 
-const Game = new ScoreBoard()
-
-Game.joinGame("Pepe")
-Game.joinGame("Juliana")
-Game.joinGame("Manuela")
-Game.joinGame("Roberto")
-Game.joinGame("Julia")
-
-Game.addOrSubstractPoints("Juliana", 10)
-Game.addOrSubstractPoints("Manuela", -2)
-Game.addOrSubstractPoints("Roberto", 0)
-Game.addOrSubstractPoints("Pepe", 5)
-
-Game.showScore()
-Game.showWinner()
-
-module.exports= ScoreBoard
+module.exports = ScoreBoard
