58e7e56fec30e3f29412e9f531f3362847964b41
Mariano <mariano_farace@hotmail.com>
2021-12-04 13:56:17 +0100

    Add singleton demonstration

--- singleton/app.js
+++ singleton/app.js
@@ -1,14 +1,13 @@
 const Player = require('./player');
 const Game = require('./game.js');
-
-
+const ScoreBoard = require('./score-board.js');
 
 //Create a new game
 const myGame = new Game()
 
 //Players are created and joined to the game, on the same line
 myGame.joinGame(new Player("Pepe"))
-myGame.joinGame(new Player("Pepe"))
+myGame.joinGame(new Player("Pepe")) //To show that a player can't join the game two times
 myGame.joinGame(new Player("Juliana"))
 myGame.joinGame(new Player("Manuela"))
 myGame.joinGame(new Player("Roberto"))
@@ -18,10 +17,12 @@ myGame.joinGame(new Player("Julia"))
 myGame.gameScoreBoard.addOrSubtractPoints("Pepe", 5) 
 myGame.gameScoreBoard.addOrSubtractPoints("Juliana", 10)
 myGame.gameScoreBoard.addOrSubtractPoints("Manuela", -2)
-myGame.gameScoreBoard.addOrSubtractPoints("Roberto", 0)
+myGame.gameScoreBoard.addOrSubtractPoints("Roberto", 1)
+
+//This demonstrates that the score board IS a singleton, since creating a new instance, returns the already existent instance created inside the new Game logic
+const newScoreBoardInstance = new ScoreBoard()
+newScoreBoardInstance.addOrSubtractPoints("Roberto", 4) //Roberto will have 5 points!
 
 //Self explanatory 
 myGame.gameScoreBoard.showScore()
 myGame.gameScoreBoard.showWinner()
-
-//TODO aca instanciar un nuevo board, para mostrar que funciona como singleton
